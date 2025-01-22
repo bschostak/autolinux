5cae4f5143e5eb5ffa9a0e0e81600c8ac16cb828
bschostak <bartek_sz02@proton.me>
2025-01-22 23:35:53 +0100

    Pre-Release 0.0.1
    1. Added nvidia packages
    2. Added nvida install scripts
    3. Added nvidia cofigurator
    3. Refactoring

--- install.sh
+++ install.sh
@@ -3,10 +3,66 @@
 #* Nvidia driver installation
 
 function choose_driver_type {
+    echo "It is recommended to tun on multilib repository for Nvidia driver installation!"
+    echo
     echo "Please choose the type of Nvidia driver you want to install:"
     echo "1) Nvidia driver"
     echo "2) Nvidia driver DKMS"
     echo "3) Quit"
+
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1) install_nvidia_driver ;;
+        2) install_nvidia_driver_dkms ;;
+        3) quit ;;
+    esac
+}
+
+function install_nvidia_driver {
+    echo "Installing Nvidia driver..."
+
+    sudo $pkg_update
+    sudo $pkg_install - < nvidia_driver/nvidia.txt
+
+    echo
+    echo "Done!"
+    configure_nvidia_driver
+}
+
+function install_nvidia_driver_dkms {
+    echo "Installing Nvidia driver DKMS..."
+
+    sudo $pkg_update
+    sudo $pkg_install - < nvidia_driver/dkms.txt
+
+    echo
+    echo "Done!"
+    configure_nvidia_driver
+}
+
+#! Use only on the Linux distributions, that require manual configuration of Nvidia driver! Arch, Artix, Gentoo, etc.
+function configure_nvidia_driver {
+    echo "Configuring Nvidia driver..."
+
+    echo "Checking if Nvidia hooks are installed..."
+    if [ -f "/etc/pacman.d/hooks/nvidia-hook.hook" ]; then
+        echo "Nvidia hooks are already installed."
+    else
+        echo "Installing Nvidia hooks..."
+        sudo cp nvidia_driver/nvidia.hook /etc/pacman.d/hooks/nvidia.hook
+    fi
+
+    echo "Blacklisting nouveau driver..."
+    if [ -f "/etc/modprobe.d/blacklist-nvidia-nouveau.conf" ]; then
+        echo "Blacklist file already exists."
+    else
+        sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+        echo "Blacklist file created."
+    fi
+
+    echo
+    echo "Done!"
 }
 
 #* Installation of packages
@@ -32,11 +88,7 @@ function install_application_packages {
 
 #* Menu options
 
-# Function definitions
 function install_pkg_list {
-    echo "You selected option 1"
-    # Add your code for option one here
-    echo
     echo "Would you like to install basic system packages or application packages?"
     echo "1) Basic system packages"
     echo "2) Application packages"
@@ -67,6 +119,7 @@ pkg_update="pacman -Syu"
 pkg_install="pacman -S"
 
 # Display menu
+
 while true; do
     echo "================================================================"
     echo "#            Welcome to the menu of automated                  #"
